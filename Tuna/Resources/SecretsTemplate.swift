5330346b0234f02cd2c2d37d285180cb3b3d3c9b
Ben Williams <benryanwilliams@hotmail.com>
2020-12-08 22:41:07 +0700

    Add secrets template

---
+++ Tuna/Resources/SecretsTemplate.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  Tuna
+//
+//  Created by Ben Williams on 08/12/2020.
+//  Copyright © 2020 Ben Williams. All rights reserved.
+//
+
+import Foundation
+
+// Rename the struct and filename to be 'Secrets.swift' and insert your API key below to use
+struct SecretsTemplate {
+    static let youtubeAPIKey = "[Insert YouTube API Key here]"
+}
