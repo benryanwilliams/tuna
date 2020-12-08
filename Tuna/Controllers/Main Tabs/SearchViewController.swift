392db9cbae39a0889423d545ee74342637104e9e
Ben Williams <benryanwilliams@hotmail.com>
2020-12-08 18:57:29 +0700

    Improve searchBar UX with dimmed view and cancel button

--- Tuna/Controllers/Main Tabs/SearchViewController.swift
+++ Tuna/Controllers/Main Tabs/SearchViewController.swift
@@ -26,6 +26,14 @@ class SearchViewController: UIViewController {
         return tableView
     }()
     
+    private let dimmedView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.isHidden = true
+        view.alpha = 0
+        return view
+    }()
+    
     // MARK:- viewDidLoad
     
     override func viewDidLoad() {
@@ -40,12 +48,16 @@ class SearchViewController: UIViewController {
         
         view.addSubview(headerView)
         view.addSubview(tableView)
+        
+        configureDimmedView()
     }
     
     // MARK:- Config
     
     private func configureSearchBar() {
         navigationController?.navigationBar.topItem?.titleView = searchBar
+        
+        searchBar.delegate = self
     }
     
     private func configureNavBarMoreButton() {
@@ -59,6 +71,19 @@ class SearchViewController: UIViewController {
     }
     
     // MARK:- Actions
+    
+    private func configureDimmedView() {
+        view.addSubview(dimmedView)
+        
+        let gesture = UITapGestureRecognizer(
+            target: self,
+            action: #selector(didFinishSearch)
+        )
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        
+        dimmedView.addGestureRecognizer(gesture)
+    }
     
     @objc private func didTapNavBarMoreButton() {
         let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -74,6 +99,8 @@ class SearchViewController: UIViewController {
             title: "Cancel",
             style: .cancel,
             handler: nil))
+        
+        actionSheet.view.tintColor = .label
         
         present(actionSheet, animated: true)
     }
@@ -96,6 +123,8 @@ class SearchViewController: UIViewController {
             width: view.width,
             height: view.height - headerView.height - (tabBarController?.tabBar.height ?? 10) - (navigationController?.navigationBar.height ?? 10)
         )
+        
+        dimmedView.frame = view.bounds
     }
 }
 
@@ -103,10 +132,69 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
+        didCancelSearch()
+        guard let text = searchBar.text, !text.isEmpty else {
+            return
+        }
+        query(with: text)
         
-        // Perform YouTube search with text (if not empty, otherwise display prompt within placeholder text)
     }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Cancel",
+            style: .plain,
+            target: self,
+            action: #selector(didCancelSearch)
+        )
+        navigationItem.rightBarButtonItem?.tintColor = .label
+        
+        dimmedView.isHidden = false
+        UIView.animate(withDuration: 0.2, animations: {
+            self.dimmedView.alpha = 0.4
+        }) { (done) in
+            if done {
+               self.tableView.isHidden = false
+            }
+        }
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        didFinishSearch()
+    }
+    
+    @objc private func didFinishSearch() {
+        searchBar.resignFirstResponder()
+        configureNavBarMoreButton()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.dimmedView.alpha = 0
+        }) { (done) in
+            if done {
+                self.dimmedView.isHidden = true
+            }
+        }
+        
+    }
+    
+    @objc private func didCancelSearch() {
+        searchBar.text = nil
+        searchBar.resignFirstResponder()
+        configureNavBarMoreButton()
+        UIView.animate(withDuration: 0.2, animations: {
+            self.dimmedView.alpha = 0
+        }) { (done) in
+            if done {
+                self.dimmedView.isHidden = true
+            }
+        }
+        
+    }
+    
+    private func query(with text: String) {
+        // Perform YouTube search with text
+        print("Search button pressed")
+    }
+    
 }
 
 // MARK:- Table View Delegate and Data Source Methods
@@ -161,7 +249,7 @@ extension SearchViewController: MoreButtonDelegate {
             style: .default,
             handler: { action in
                 // Add to array of models within library and display a message that automatically disappears saying that it has successfully been added (otherwise, show an error message)
-        }))
+            }))
         
         actionSheet.addAction(UIAlertAction(
             title: "Copy link",
@@ -175,6 +263,8 @@ extension SearchViewController: MoreButtonDelegate {
             style: .cancel,
             handler: nil))
      
+        actionSheet.view.tintColor = .label
+        
         present(actionSheet, animated: true, completion: nil)
         
     }
